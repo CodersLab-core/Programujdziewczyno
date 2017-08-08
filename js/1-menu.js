b25a9deb973f488bc0c94cfcfd1966c71d26efc3
joannabochynska <joanna.bochynska@gmail.com>
2017-08-08 22:53:53 +0200

    Dropdown menu

---
+++ js/1-menu.js
@@ -0,0 +1,16 @@
+'use strict';
+
+document.addEventListener('DOMContentLoaded', function() {
+
+  // Dropdown menu in navigation
+  var elementWithDropdown = document.querySelector('.for-dropdown');
+  var dropdown = document.querySelector('.dropdown');
+
+  elementWithDropdown.addEventListener('mouseover', function() {
+    dropdown.style.display = "block";
+  }); 
+
+  elementWithDropdown.addEventListener('mouseout', function() {
+    dropdown.style.display = "none";
+  });
+});
