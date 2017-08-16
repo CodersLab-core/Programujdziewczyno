fd53cf6c5fb607d6dab459447d437e3b4b4fb5b1
joannabochynska <joanna.bochynska@gmail.com>
2017-08-16 20:41:54 +0200

    dropdown menu with updates

--- js/1-menu.js
+++ js/1-menu.js
@@ -2,15 +2,20 @@
 
 document.addEventListener('DOMContentLoaded', function() {
 
+<<<<<<< HEAD
   // Dropdown menu in navigation
+=======
+  //Dropdown menu in navigation
+
+>>>>>>> dropdown menu with updates
   var elementWithDropdown = document.querySelector('.for-dropdown');
   var dropdown = document.querySelector('.dropdown');
 
   elementWithDropdown.addEventListener('mouseover', function() {
-    dropdown.style.display = "block";
+    dropdown.style.display = 'block';
   }); 
 
   elementWithDropdown.addEventListener('mouseout', function() {
-    dropdown.style.display = "none";
+    dropdown.style.display = 'none';
   });
 });
