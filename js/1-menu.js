0397b555153b9fb8db53a50e133f7ee9cd88803f
joannabochynska <joanna.bochynska@gmail.com>
2017-08-27 15:23:37 +0200

    changes from master, updates

--- js/1-menu.js
+++ js/1-menu.js
@@ -2,12 +2,8 @@
 
 document.addEventListener('DOMContentLoaded', function() {
 
-<<<<<<< HEAD
   // Dropdown menu in navigation
-=======
-  //Dropdown menu in navigation
 
->>>>>>> dropdown menu with updates
   var elementWithDropdown = document.querySelector('.for-dropdown');
   var dropdown = document.querySelector('.dropdown');
 
