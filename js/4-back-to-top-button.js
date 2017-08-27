36a252c383d1d1ce1d897314bcfe5c0329a7797a
joannabochynska <joanna.bochynska@gmail.com>
2017-08-27 15:45:14 +0200

    changes from master

--- js/4-back-to-top-button.js
+++ js/4-back-to-top-button.js
@@ -3,7 +3,6 @@
 document.addEventListener('DOMContentLoaded', function() {
 
   // Back to top button
-
   var backToTopBtn = document.querySelector('.back-to-top');
 
   backToTopBtn.addEventListener('click', function() {
